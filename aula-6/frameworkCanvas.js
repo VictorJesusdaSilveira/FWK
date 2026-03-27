9f2802f1f0877394111b0adde6e63a14a941575a
Victor J. da Silveira <victorj.ifpr2024@gmail.com>
2026-03-27 09:04:14 -0300

    Update frameworkCanvas.js

--- aula-6/frameworkCanvas.js
+++ aula-6/frameworkCanvas.js
@@ -2,22 +2,24 @@ onst body = document.getElementsByTagName("body")[0];
 const canvas = document.createElement("canvas");
 body.appendChild(canvas);
 const ctx = canvas.getContext("2d");
+const arco = document.querySelector("circulo")
 
 const circulo = {
 		x:100,
 		y:100,
 		r:50,
-		desenhar:function(){
+		desenhar:function(raio){
 			ctx.beginPath();
-			ctx.arc(this.x , this.y , this.r , 0 , 2 * Math.PI);
+			ctx.arc(this.x , this.y , raio , 0 , 2 * Math.PI);
 			ctx.fillSTyle = "#000000";
 			ctx.fill();
 			ctx.closePath();
 	}
 };	
 
-if(document.querySelector("circulo")){
-	circulo.desenhar();
+if(arco)){
+	let raio = arco.getAttribute("raio");
+	circulo.desenhar(raio);
 }
 
 
