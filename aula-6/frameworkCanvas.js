88ede70ea171d86d7f5386f12bdf928df045ff6e
Victor J. da Silveira <victorj.ifpr2024@gmail.com>
2026-03-27 08:51:25 -0300

    Create frameworkCanvas.js

---
+++ aula-6/frameworkCanvas.js
@@ -0,0 +1,17 @@
+onst body = document.getElementsByTagName("body")[0];
+const canvas = document.createElement("canvas");
+body.appendChild(canvas);
+const ctx = canvas.getContext("2d");
+
+const circulo = {
+	x:100,
+	y:100,
+	r:50,
+	desenhar:function(){
+		ctx.beginPath();
+		ctx.arc(this.x , this.y , this.r , 0 , 2 * Math.PI);
+		ctx.fillSTyle = "#000000";
+		ctx.fill
+	}
+
+}						
