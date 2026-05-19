c85189d78cf02910f323c9591521a5bb79b0df08
Victor J. da Silveira <victorj.ifpr2024@gmail.com>
2026-05-19 11:43:59 -0300

    Create banco.php

---
+++ Bimestre2/aula4/banco.php
@@ -0,0 +1,11 @@
+<?php
+$entidades = [
+   "nome"=>["aluno","professor","curso"]
+ ];
+$atributos = [
+    "aluno"=>["nome","idade","nascimento"],
+    "professor"=>["nome","idade","titulacao"],
+    "curso"=>["nome","turno","anos"],
+];
+
+?>
